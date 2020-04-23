35eee96c45222319045b8f6dc0755f8b5b31f8a6
alshap <40171499+alshap@users.noreply.github.com>
2020-04-23 17:57:28 +0100

    Add files via upload

--- face_recognition.py
+++ face_recognition.py
@@ -3,13 +3,13 @@ import numpy as np
 
 cap = cv2.VideoCapture(0)
 
-face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+face_cascade_path = '/home/pi/Desktop/project_peopleRecognition/haarcascade_frontalface_default.xml'
+face_cascade = cv2.CascadeClassifier(face_cascade_path)
 
 while True:
     _, frame = cap.read()
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    print(face_cascade)
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
     for rect in faces:
         (x, y, w, h) = rect
@@ -19,7 +19,8 @@ while True:
     cv2.imshow("Frame", frame)
 
     key = cv2.waitKey(1)
-    if key == 27:
+#   key Esc
+    if key == 27: 
         break
 
 cap.release()
